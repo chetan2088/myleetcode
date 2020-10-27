25d187a9508a484e1ed1702dfc3e7a33d9e42d8f
Chetan Anand <chetanguptasarai@gmail.com>
2020-10-27 17:00:47 +0530

    ok

---
+++ exercise 1/example1.js
@@ -0,0 +1,20 @@
+//  var runningSum = function(nums) {
+//    if (nums.length === 0)
+//        return []
+//    var output = [nums[0]]
+//    for (var i = 1; i < nums.length; i++) {
+//        output.push(nums[i] + output[output.length - 1])
+//    }
+//    return output
+//    };
+
+    
+    var runningSum = function(nums) {
+    if (nums.length === 0)
+        return []
+    var output = [nums[0]]
+    for (var i = 1; i < nums.length; i++) {
+        output.push(nums[i] + output[output.length - 1])
+    }
+    return output
+};
